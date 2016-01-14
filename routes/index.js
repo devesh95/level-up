4e9b30eb1912abaf2af12f413516966ae79bff48
Devesh Dayal <devesh95@gmail.com>
2016-01-14 09:29:26 -0500

    fix for gmail emails

--- routes/index.js
+++ routes/index.js
@@ -47,8 +47,9 @@ router.get('/register', function(req, res) {
 
 router.post('/register', function(req, res) {
   if (req.body && req.body.firstname && req.body.lastname && req.body.school && req.body.email) {
+    var email = req.body.email.toLowerCase().replace(/+/g, '').replace(/./g, '');
     Account.find({
-      email: req.body.email.toLowerCase()
+      email: email
     }, function(err, existingAccountWithSameEmail) {
       if (err) {
         console.log(err);
@@ -64,7 +65,7 @@ router.post('/register', function(req, res) {
           Account.register(new Account({
             username: req.body.username,
             firstname: req.body.firstname,
-            email: req.body.email.toLowerCase(),
+            email: email,
             lastname: req.body.lastname,
             school: req.body.school,
             current_level: 0
